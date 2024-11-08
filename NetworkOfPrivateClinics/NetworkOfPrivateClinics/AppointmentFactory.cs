30c3c8c9e4d8312cdb353a0b2f0694ac2bf28a26
OlehKozlovskyi <oleg.koylovskiy.15-p@ukr.net>
2024-11-08 23:12:12 +0200

    Implement DailyRoutime class that will use for AppointmentsFactory

---
+++ NetworkOfPrivateClinics/NetworkOfPrivateClinics/AppointmentFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkOfPrivateClinics
+{
+    public class AppointmentFactory
+    {
+        public AppointmentFactory(string startWorkingDay, string endWorkingDay)
+        {
+            
+        }
+
+        public Dictionary<int, DailyRoutine> Appointment { get; set; }
+
+        
+
+    }
+}
---
+++ NetworkOfPrivateClinics/NetworkOfPrivateClinics/DailyRoutine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkOfPrivateClinics
+{
+    public class DailyRoutine
+    {
+        public Dictionary<TimeOnly, Patient> ListOfDailyAppointments { get; set; }
+
+        public DailyRoutine(string startWorkingDay, string endWorkingDay)
+        {
+            StartWorkingDay = ConvertToTimeOnly(startWorkingDay);
+            EndWorkingDay = ConvertToTimeOnly(endWorkingDay);
+            ListOfDailyAppointments = GenerateDailyRoutine(StartWorkingDay, EndWorkingDay);
+        }
+
+        public readonly TimeOnly StartWorkingDay;
+        public readonly TimeOnly EndWorkingDay;
+
+        private TimeOnly ConvertToTimeOnly(string time) => TimeOnly.Parse(time, new CultureInfo("uk-UA"));
+
+        private Dictionary<TimeOnly, Patient> GenerateDailyRoutine(TimeOnly startWorkingDay, TimeOnly endWorkingDay)
+        {
+            TimeOnly currentTime = startWorkingDay;
+            Dictionary<TimeOnly, Patient> generatedList = new();
+            while (currentTime < endWorkingDay)
+            {
+                generatedList.Add(currentTime, null);
+                currentTime = currentTime.AddHours(1);
+            }
+            return generatedList;
+        }
+    }
+}
