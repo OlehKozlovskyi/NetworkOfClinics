b37adc776ed9414ecd53c3fa8a947f3baa39cbae
OlehKozlovskyi <oleg.koylovskiy.15-p@ukr.net>
2024-11-22 21:26:35 +0200

    Implement FileWriterFactory

---
+++ NetworkOfPrivateClinics/NetworkOfPrivateClinics/CustomExceptions/UnsupportedFileException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkOfPrivateClinics.CustomExceptions
+{
+    public class UnsupportedFileException:Exception
+    {
+        public UnsupportedFileException(string file)
+            :base(string.Format($"Invalid file extension: {file}")){ }
+    }
+}
---
+++ NetworkOfPrivateClinics/NetworkOfPrivateClinics/WorkingWithFiles/FileWriterFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using NetworkOfPrivateClinics.CustomExceptions;
+
+namespace NetworkOfPrivateClinics.WorkingWithFiles
+{
+    public class FileWriterFactory
+    {
+        public BaseWriter GetFileWriter(string path)
+        {
+            var fileExtension = Path.GetExtension(path);
+            switch (fileExtension)
+            {
+                case ".csv":
+                    return new CsvFileWriter(path);
+                case ".json":
+                    return new JsonFileWriter(path);
+                default:
+                    throw new UnsupportedFileException(Path.GetFileName(path));
+            }
+        }
+    }
+}
