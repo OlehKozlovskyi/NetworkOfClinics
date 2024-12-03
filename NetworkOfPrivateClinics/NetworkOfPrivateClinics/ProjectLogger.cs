83ddce68e1344ae1ef9bb7fa173ad4cb834acfe7
OlehKozlovskyi <oleg.koylovskiy.15-p@ukr.net>
2024-12-03 23:46:30 +0200

    Implement Logger

---
+++ NetworkOfPrivateClinics/NetworkOfPrivateClinics/ProjectLogger.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Logging;
+using NetworkOfPrivateClinics.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkOfPrivateClinics
+{
+    public class ProjectLogger<T> : ICustomLogger
+    {
+        private static ILogger<T> _logger;
+
+        public ProjectLogger()
+        {
+            using var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddConsole();
+                builder.SetMinimumLevel(LogLevel.Information);
+            });
+            _logger = loggerFactory.CreateLogger<T>();
+        }
+
+        public async Task LogInformation(string message)
+        {
+            await Task.Run(()=>_logger.LogInformation(message));
+        }
+
+        public async Task LogWarning(string message)
+        {
+            await Task.Run(() => _logger.LogWarning(message));
+        }
+
+        public async Task LogError(string message)
+        {
+            await Task.Run(() => _logger.LogError(message));
+        }
+    }
+}
---
+++ NetworkOfPrivateClinics/NetworkOfPrivateClinics/Services/ICustomLogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkOfPrivateClinics.Services
+{
+    public interface ICustomLogger
+    {
+        Task LogInformation(string message);
+        Task LogWarning(string message);
+        Task LogError(string message);
+    }
+}
